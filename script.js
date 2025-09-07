f800684f112677fd65db3f128e9d2a8b559322ee
Ravi <rg236343@gmail.com>
2025-09-07 23:26:43 +0530

    file operations

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const fs = require('fs');
+const fs = require('node:fs');
 
-const ab = fs.readFileSync('myfile.txt', 'utf-8');
+// let  data = fs.readFileSync('myfile.txt', 'utf-8');
 
 console.clear();
-console.warn(ab);
+// console.log('Before edit - \n');
+// console.log(data);
+
+// fs.writeFileSync('myfile2.txt', 'Hello raviiii', 'utf-8');
+// fs.appendFileSync('myfile2.txt', '\n Hello Ravi', 'utf-8');
+
+// fs.mkdirSync('games')
+// fs.mkdirSync('test/tes2/t', {recursive:true})
+// fs.unlinkSync('myfile.txt');
+// fs.rmdirSync('test', {recursive:true});
+data = fs.readFileSync('myfile2.txt', 'utf-8');
+// console.log('After edit - \n');
+console.log(data);
+
