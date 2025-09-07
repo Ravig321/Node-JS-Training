9cfa171ca1d570293822748ef205febf68dffa8c
Ravi <rg236343@gmail.com>
2025-09-07 23:37:55 +0530

    async functions on file read

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const fs = require('node:fs');
 
+
+// SYNC -- Blocking Operations
+
 // let  data = fs.readFileSync('myfile.txt', 'utf-8');
 
-console.clear();
+// console.clear();
 // console.log('Before edit - \n');
 // console.log(data);
 
@@ -13,7 +16,26 @@ console.clear();
 // fs.mkdirSync('test/tes2/t', {recursive:true})
 // fs.unlinkSync('myfile.txt');
 // fs.rmdirSync('test', {recursive:true});
-data = fs.readFileSync('myfile2.txt', 'utf-8');
+// data = fs.readFileSync('myfile2.txt', 'utf-8');
 // console.log('After edit - \n');
-console.log(data);
+// console.log(data);
+
+
+
+
+
+// ------------------------------------------
+
+// Without Sync ------- Non- Blockinng Operations
+console.log('Script Execution Started');
+
+let data = fs.readFile('myfile2.txt', 'utf-8', function(error, data){
+    if(error){
+        console.log(`error occured-- ${error}`);
+    }else{
+        console.log(`Content -- ${data}`);
+    }
+})
+
+console.log('Script Executed');
 
